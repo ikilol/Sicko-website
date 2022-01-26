bb1064982b6431f7f9897e1ae1169652afe28c40
ikilol <87668373+ikilol@users.noreply.github.com>
2022-01-26 15:52:36 +0100

    grafico 02

--- sketch2.js
+++ sketch2.js
@@ -1,11 +1,17 @@
 let serial; // variable for the serial object
 let latestData = 0; // variable to hold the data
 let ok = 1;
+let arr = [90, 88, 40, 83, 99];
+//let bigick = arr[0];
+let bigick = 231;
 
 let phase = 0;
 let zoff = 0;
 let slider;
 let slider1;
+//let tempo = 0-700;
+let totalicks = 2340;
+let newestData = 0;
 
 
 function setup() {
@@ -16,7 +22,7 @@ function setup() {
   // get a list of all connected serial devices
   serial.list();
   // serial port to use - you'll need to change this
-  serial.open('COM3');
+  serial.open('COM6');
   // callback for when the sketchs connects to the server
   serial.on('connected', serverConnected);
   // callback to print the list of serial devices
@@ -72,32 +78,97 @@ function gotData() {
 function draw() {
   background(255);
 
-  if (latestData <= 10){
+ // for (var i = 0; i < arr.length; i++){
+ //   if (arr[i] >= bigick) {
+ //     bigick = arr[i];
+ //   }
+ // }
+//
+ // console.log(bigick);
+
+  if (latestData >= 1000 && latestData <2000){
+  textSize(20);
+  fill(0);
+  noStroke();
+  textStyle(NORMAL);
+  textAlign(LEFT, CENTER);
+  text("Total number\nof icks collected", 650, 100);
+
+  textSize(40);
+  textStyle(BOLD);
+  text(totalicks, 650, 150);
+
+  textSize(20);
+  textStyle(NORMAL);
+  fill(0);
+  noStroke();
+  textAlign(LEFT);
+  text("highest amount\nof icks per session", 650, 250);
+
+  textSize(40);
+  textStyle(BOLD);
+  text(bigick, 650, 300);
+
+  newestData = latestData - 1000;
+
+  }
+
+  if (latestData >= 2000){
+    textSize(20);
+    fill(0);
+    noStroke();
+    textStyle(NORMAL);
+    textAlign(LEFT, CENTER);
+    text("Total number\nof icks collected", 650, 100);
+  
+    textSize(40);
+    textStyle(BOLD);
+    text(totalicks, 650, 150);
+  
+    textSize(20);
+    textStyle(NORMAL);
+    fill(0);
+    noStroke();
+    textAlign(LEFT);
+    text("Time left", 650, 250);
+  
+    textSize(40);
+    textStyle(BOLD);
+    text(tempo, 650, 300);
+  
+    newestData = latestData - 2000;
+  
+    }
+  
+
+
+
+  if (newestData <= 10){
     slider = createSlider(0, 10, 0.7, 0.1);
     slider1 = createSlider(0, 10, 0.4, 0.1);
 
     translate(random(-1),random(-1));
     }
 
-  if (latestData > 10 && latestData < 20){
+  if (newestData > 10 && newestData < 20){
     slider = createSlider(0, 10, 1, 0.1);
     slider1 = createSlider(0, 10, 2, 0.1);
     translate(random(-2),random(-2));
     }
   
-    if (latestData > 20 && latestData < 80){
+    if (newestData > 20 && newestData < 80){
       slider = createSlider(15, 10, 3, 0.1);
       slider1 = createSlider(16, 10, 4, 0.1);
       translate(random(-3),random(-3));
     }
   
-    if (latestData > 80 && latestData < 200){
+    if (newestData > 80 && newestData < 200){
       slider = createSlider(25, 10, 5, 0.1);
       slider1 = createSlider(36, 10, 6, 0.1);
       translate(random(-4),random(-4));
     }
   
-    if (latestData >= 200){
+    if (newestData >= 200){
       slider = createSlider(50, 10, 10, 0.1);
       slider1 = createSlider(100, 10, 10, 0.1);
       translate(random(-5),random(-5));
@@ -106,7 +177,7 @@ function draw() {
     slider.hide();
     slider1.hide();
 
-  translate(width / 2, height / 2);
+  translate(width / 2.7, height / 2);
   stroke(0);
   strokeWeight(2);
   noFill();
@@ -141,8 +212,28 @@ function draw() {
 
   translate(random(0),random(-0));
   textSize(40);
-  fill(50);
+  fill(0);
   noStroke();
   textAlign(CENTER, CENTER);
-  text(latestData + " icks", 0, 0);
+  text(newestData + " icks", 0, 0);
+
+
+
+ // if (latestData=800){
+ //   sessione = 0
+ // }
+//
+ // if (latestData=710){
+ //   tempo = 10
+ // }
+//
+ // if (latestData=709){
+ //   tempo = 9
+ // }
+//
+ // if (latestData=708){
+ //   tempo = 8
+ // }
+
+
 }
